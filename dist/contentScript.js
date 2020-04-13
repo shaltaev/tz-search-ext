a835f3f2e4b2319fe3cab3dd7649d7c4b0e9bad1
shaltaev <shaltaev.gleb@gmail.com>
2020-04-13 17:38:35 +0300

    ready

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('12', Date.now());
+var SEARCH_TEXT = 'ООО "Восточный"';
+if (window && window.location.href.includes('google')) {
+    var inputEl = document.querySelector('input[aria-label="Search"]') ||
+        document.querySelector('input[aria-label="Найти"]') ||
+        undefined;
+    if ((inputEl === null || inputEl === void 0 ? void 0 : inputEl.value) !== '') {
+        var searchEl = document.querySelector('div#search') || undefined;
+        if (searchEl) {
+            var items = document.querySelectorAll('div.g');
+            items.forEach(function (item) {
+                var title = item.querySelector('h3');
+                var description = item.querySelector('h3');
+                if ((title === null || title === void 0 ? void 0 : title.innerText.toLowerCase().includes(SEARCH_TEXT.toLowerCase())) || (description === null || description === void 0 ? void 0 : description.innerText.toLowerCase().includes(SEARCH_TEXT.toLowerCase()))) {
+                    var anchor = item.querySelector('div.r > a');
+                    if (anchor) {
+                        anchor.click();
+                    }
+                }
+            });
+        }
+    }
+}
