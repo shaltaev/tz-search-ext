03cf4b9fee76848630578b1bda3dfb0c3878974d
shaltaev <shaltaev.gleb@gmail.com>
2020-04-14 14:30:19 +0300

    0.0.4 refactoring

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -4,22 +4,26 @@ if (window && window.location.href.includes('google')) {
         document.querySelector('input[aria-label="Найти"]') ||
         undefined;
     if ((inputEl_1 === null || inputEl_1 === void 0 ? void 0 : inputEl_1.value) !== '') {
-        var searchEl = document.querySelector('div#search') || undefined;
-        if (searchEl) {
-            var items = document.querySelectorAll('div.g');
-            items.forEach(function (item) {
-                var title = item.querySelector('h3');
-                var description = item.querySelector('span.st');
-                chrome.storage.sync.get('toGoField', function (data) {
-                    var search = data.toGoField || '';
-                    if (search.length > 1 && ((title === null || title === void 0 ? void 0 : title.innerText.toLowerCase().includes(search.toLowerCase())) || (description === null || description === void 0 ? void 0 : description.innerText.toLowerCase().includes(search.toLowerCase())))) {
-                        var anchor = item.querySelector('div.r > a');
-                        if (anchor) {
-                            anchor.click();
+        chrome.storage.sync.get('toGoField', function (data) {
+            var search = data.toGoField || '';
+            if (search.length > 1) {
+                var searchEl = document.querySelector('div#search') || undefined;
+                if (searchEl) {
+                    var items = document.querySelectorAll('div.g');
+                    items.forEach(function (item) {
+                        var title = item.querySelector('h3');
+                        var description = item.querySelector('span.st');
+                        var isInTitle = (title === null || title === void 0 ? void 0 : title.innerText.toLowerCase().includes(search.toLowerCase())) || false;
+                        var isInDescription = (description === null || description === void 0 ? void 0 : description.innerText.toLowerCase().includes(search.toLowerCase())) || false;
+                        if (isInTitle || isInDescription) {
+                            var anchor = item.querySelector('div.r > a');
+                            if (anchor) {
+                                anchor.click();
+                            }
                         }
-                    }
-                });
-            });
-        }
+                    });
+                }
+            }
+        });
     }
 }
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -3,36 +3,31 @@ if (window && window.location.href.includes('google')) {
         document.querySelector<HTMLInputElement>('input[aria-label="Search"]') ||
         document.querySelector<HTMLInputElement>('input[aria-label="Найти"]') ||
         undefined
-
     if (inputEl?.value !== '') {
-        const searchEl: HTMLDivElement | undefined =
-            document.querySelector<HTMLDivElement>('div#search') || undefined
+        chrome.storage.sync.get('toGoField', (data) => {
+            const search = data.toGoField || ''
+            if (search.length > 1) {
+                const searchEl: HTMLDivElement | undefined =
+                    document.querySelector<HTMLDivElement>('div#search') || undefined
 
-        if (searchEl) {
-            const items = document.querySelectorAll('div.g')
-            items.forEach((item) => {
-                const title = item.querySelector<HTMLHeadingElement>('h3')
-                const description = item.querySelector<HTMLSpanElement>('span.st')
+                if (searchEl) {
+                    const items = document.querySelectorAll('div.g')
+                    items.forEach((item) => {
+                        const title = item.querySelector<HTMLHeadingElement>('h3')
+                        const description = item.querySelector<HTMLSpanElement>('span.st')
+                        const isInTitle: boolean = title?.innerText.toLowerCase().includes(search.toLowerCase()) || false
+                        const isInDescription: boolean = description?.innerText
+                            .toLowerCase()
+                            .includes(search.toLowerCase()) || false
 
-                chrome.storage.sync.get('toGoField', (data) => {
-                    const search = data.toGoField || ''
-                    if (
-                        search.length > 1 && (
-                            title?.innerText.toLowerCase().includes(search.toLowerCase()) ||
-                            description?.innerText
-                                .toLowerCase()
-                                .includes(search.toLowerCase())
-                        )
-                    ) {
-                        const anchor = item.querySelector<HTMLAnchorElement>('div.r > a')
+                        if ( isInTitle || isInDescription ) {
+                            const anchor = item.querySelector<HTMLAnchorElement>('div.r > a')
 
-                        if (anchor) {
-                            anchor.click()
+                            if (anchor) {anchor.click()}
                         }
-                    }
-                })
-
-            })
-        }
+                    })
+                }
+            }
+        })
     }
 }
